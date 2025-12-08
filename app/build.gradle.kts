50be01167255e7ff096b1537b375e21abf4e4d5b
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-12-08 00:45:17 -0300

    Fix null safety for release store file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(project.properties["RELEASE_STORE_FILE"])
+            storeFile = file(project.properties["RELEASE_STORE_FILE"]!!)
             storePassword = project.properties["RELEASE_STORE_PASSWORD"] as String
             keyAlias = project.properties["RELEASE_KEY_ALIAS"] as String
             keyPassword = project.properties["RELEASE_KEY_PASSWORD"] as String
